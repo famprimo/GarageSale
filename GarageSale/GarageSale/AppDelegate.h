bd5a34f5a5dca431c2da53d0941494f682ccebf1
Freddy Amprimo <famprimo@yahoo.com>
2014-05-17 10:29:12 -0500

    Initial Commit

---
+++ GarageSale/GarageSale/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GarageSale
+//
+//  Created by Federico Amprimo on 17/05/14.
+//  Copyright (c) 2014 Federico Amprimo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
